7713944054d271da0b670fadbf12093b929a516d
RodionovNikita <radzivonaumikita@gmail.com>
2026-06-23 12:22:03 +0200

    added new service

---
+++ src/main/java/com/example/paymentservice/application/service/AuditService.java
@@ -0,0 +1,28 @@
+package com.example.paymentservice.application.service;
+
+import com.example.paymentservice.domain.payment.PaymentStatus;
+import com.example.paymentservice.domain.paymentHistory.PaymentHistory;
+import com.example.paymentservice.infrastructure.persistence.repository.PaymentHistoryRepositoryImpl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class AuditService {
+
+    private final PaymentHistoryRepositoryImpl repository;
+
+    public void record(UUID paymentId, PaymentStatus oldStatus, PaymentStatus newStatus) {
+        repository.save(new PaymentHistory(
+                UUID.randomUUID(),
+                paymentId,
+                oldStatus,
+                newStatus,
+                LocalDateTime.now()
+        ));
+    }
+}
