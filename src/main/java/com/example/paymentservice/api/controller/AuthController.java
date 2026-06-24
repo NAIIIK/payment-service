eee030d3f88e20894f1a6313bedaa64c1aa0174b
RodionovNikita <radzivonaumikita@gmail.com>
2026-06-24 21:42:35 +0200

    added AuthController.java and following records

---
+++ src/main/java/com/example/paymentservice/api/controller/AuthController.java
@@ -0,0 +1,41 @@
+package com.example.paymentservice.api.controller;
+
+import com.example.paymentservice.api.record.AuthResponse;
+import com.example.paymentservice.api.record.LoginRequest;
+import com.example.paymentservice.api.record.RegisterRequest;
+import com.example.paymentservice.application.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public AuthResponse register(@RequestBody @Valid RegisterRequest request) {
+        String token = authService.register(
+                request.username(),
+                request.password(),
+                request.email()
+        );
+
+        return new AuthResponse(token);
+    }
+
+    @PostMapping("/login")
+    public AuthResponse login(@RequestBody @Valid LoginRequest request) {
+        String token = authService.login(
+                request.username(),
+                request.password()
+        );
+
+        return new AuthResponse(token);
+    }
+}
---
+++ src/main/java/com/example/paymentservice/api/record/AuthResponse.java
@@ -0,0 +1,3 @@
+package com.example.paymentservice.api.record;
+
+public record AuthResponse(String token) {}
