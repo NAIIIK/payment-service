605062cc0070a597e9a137f73ed438bdc1996b7c
RodionovNikita <radzivonaumikita@gmail.com>
2026-08-11 10:18:20 +0200

    change flyway migration file: add stripe_payment_intent_id column

--- src/main/resources/db/migration/V1__create_payments_table.sql
+++ src/main/resources/db/migration/V1__create_payments_table.sql
@@ -5,6 +5,7 @@ CREATE TABLE payments (
     amount          NUMERIC(19, 4) NOT NULL,
     currency        VARCHAR(3) NOT NULL,
     status          VARCHAR(20) NOT NULL,
+    stripe_payment_intent_id VARCHAR(255),
     version         BIGINT DEFAULT 0 NOT NULL,
     created_at      TIMESTAMP NOT NULL
 );
