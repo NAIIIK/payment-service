a91e74771392eabaeedac58bdc17300a5a70558c
RodionovNikita <radzivonaumikita@gmail.com>
2026-08-12 10:18:30 +0200

    add new env variable for StripeConfig

--- src/main/java/com/example/payment_service/infrastructure/config/StripeConfig.java
+++ src/main/java/com/example/payment_service/infrastructure/config/StripeConfig.java
@@ -2,6 +2,7 @@ package com.example.payment_service.infrastructure.config;
 
 import com.stripe.Stripe;
 import jakarta.annotation.PostConstruct;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +11,10 @@ public class StripeConfig {
 
     @Value("${stripe.secret-key}")
     private String secretKey;
+
+    @Getter
+    @Value("${stripe.webhook-secret}")
+    private String webhookSecret;
 
     @PostConstruct
     public void init() {
