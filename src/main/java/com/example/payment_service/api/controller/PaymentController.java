95bac4ffed12fa312f6b38bab29e967d31b6419f
RodionovNikita <radzivonaumikita@gmail.com>
2026-08-12 10:23:44 +0200

    remove process webhook, add authorities check for payment status changing webhooks

--- src/main/java/com/example/payment_service/api/controller/PaymentController.java
+++ src/main/java/com/example/payment_service/api/controller/PaymentController.java
@@ -3,9 +3,11 @@ package com.example.payment_service.api.controller;
 import com.example.payment_service.api.dto.PaymentCreationRequest;
 import com.example.payment_service.api.dto.PaymentResponse;
 import com.example.payment_service.application.service.PaymentService;
+import com.example.payment_service.application.service.dto.PaymentCreationResult;
 import com.example.payment_service.infrastructure.idempotency.Idempotent;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -25,20 +27,19 @@ public class PaymentController {
     @PostMapping
     @Idempotent
     public PaymentResponse createPayment(@Valid @RequestBody PaymentCreationRequest request) {
-        return PaymentResponse.from(paymentService.create(request));
-    }
+        PaymentCreationResult result = paymentService.create(request);
 
-    @PatchMapping("/{id}/process")
-    public PaymentResponse processPayment(@PathVariable UUID id) {
-        return PaymentResponse.from(paymentService.process(id));
+        return PaymentResponse.from(result.payment(), result.clientSecret());
     }
 
     @PatchMapping("/{id}/complete")
+    @PreAuthorize("hasRole('ADMIN')")
     public PaymentResponse completePayment(@PathVariable UUID id) {
         return PaymentResponse.from(paymentService.complete(id));
     }
 
     @PatchMapping("/{id}/fail")
+    @PreAuthorize("hasRole('ADMIN')")
     public PaymentResponse failPayment(@PathVariable UUID id) {
         return PaymentResponse.from(paymentService.fail(id));
     }
