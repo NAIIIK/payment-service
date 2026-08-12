51ee60d5d1b2d60ac037e91fbd3ad4590c5ed3bb
RodionovNikita <radzivonaumikita@gmail.com>
2026-08-12 10:21:34 +0200

    refactor InvalidPaymentStatusException

--- src/main/java/com/example/payment_service/domain/exception/InvalidPaymentStatusException.java
+++ src/main/java/com/example/payment_service/domain/exception/InvalidPaymentStatusException.java
@@ -3,7 +3,7 @@ package com.example.payment_service.domain.exception;
 import com.example.payment_service.domain.payment.PaymentStatus;
 
 public class InvalidPaymentStatusException extends RuntimeException {
-  public InvalidPaymentStatusException(PaymentStatus expected, PaymentStatus current) {
-    super("Payment status must be " + expected + ". Current status is " + current);
+  public InvalidPaymentStatusException(PaymentStatus current) {
+    super("Payment status must be PENDING. Current status is " + current);
   }
 }
