6dc698387fa2772040a4d73a4b457c5ccf6cfd7f
RodionovNikita <radzivonaumikita@gmail.com>
2026-08-12 14:26:24 +0200

    remove unused import

--- src/main/java/com/example/payment_service/infrastructure/security/UserDetailsServiceImpl.java
+++ src/main/java/com/example/payment_service/infrastructure/security/UserDetailsServiceImpl.java
@@ -2,7 +2,6 @@ package com.example.payment_service.infrastructure.security;
 
 import com.example.payment_service.domain.user.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
