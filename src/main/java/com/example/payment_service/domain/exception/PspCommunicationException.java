9ff43eda8cfa321cba94f4a5f55ec8f88b1be09d
RodionovNikita <radzivonaumikita@gmail.com>
2026-08-12 19:09:05 +0200

    add new custom PspCommunicationException

---
+++ src/main/java/com/example/payment_service/domain/exception/PspCommunicationException.java
@@ -0,0 +1,7 @@
+package com.example.payment_service.domain.exception;
+
+public class PspCommunicationException extends RuntimeException {
+    public PspCommunicationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
