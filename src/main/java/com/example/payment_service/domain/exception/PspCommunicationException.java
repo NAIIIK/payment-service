d7495aac7ab15eca5f3faaec8c9f0184b4daf975
RodionovNikita <radzivonaumikita@gmail.com>
2026-08-13 15:15:07 +0200

    add new constructor

--- src/main/java/com/example/payment_service/domain/exception/PspCommunicationException.java
+++ src/main/java/com/example/payment_service/domain/exception/PspCommunicationException.java
@@ -1,6 +1,8 @@
 package com.example.payment_service.domain.exception;
 
 public class PspCommunicationException extends RuntimeException {
+    public PspCommunicationException(String message) {super(message);}
+
     public PspCommunicationException(String message, Throwable cause) {
         super(message, cause);
     }
