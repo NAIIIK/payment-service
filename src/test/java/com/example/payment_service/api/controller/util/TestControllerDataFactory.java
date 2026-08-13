0badaac1974f0a2448f9a1aee96b03060e1c9a35
RodionovNikita <radzivonaumikita@gmail.com>
2026-08-13 14:15:46 +0200

    add TestControllerDataFactory util class

---
+++ src/test/java/com/example/payment_service/api/controller/util/TestControllerDataFactory.java
@@ -0,0 +1,8 @@
+package com.example.payment_service.api.controller.util;
+
+public final class TestControllerDataFactory {
+
+    private TestControllerDataFactory() {}
+
+    public static final String PAYMENT_INTENT_ID = "pi_test_123";
+}
