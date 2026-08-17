08e4208a957ed2f3d89b5c08232f1f288fa9f419
RodionovNikita <radzivonaumikita@gmail.com>
2026-08-17 09:25:17 +0200

    add logging when JWT toke is invalid

--- src/main/java/com/example/payment_service/infrastructure/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/payment_service/infrastructure/security/JwtAuthenticationFilter.java
@@ -7,6 +7,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,6 +18,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
@@ -54,7 +56,9 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                     SecurityContextHolder.getContext().setAuthentication(authToken);
                 }
             }
-        } catch (JwtException ignored) {}
+        } catch (JwtException ignored) {
+            log.debug("JWT Token is invalid");
+        }
 
         filterChain.doFilter(request, response);
     }
